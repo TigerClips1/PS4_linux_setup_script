7fa976f5a5ed9ebd0e9d7321e49df6490f533eea
TigerClips1 <tigerclips1@localhost.localdomain>
2023-05-08 16:06:23 -0400

    UPDATE

--- PS4Linux_fedora_setup.sh
+++ PS4Linux_fedora_setup.sh
@@ -5,7 +5,7 @@ echo "running the script"
 
 #first command
 sudo dnf update -x kernel*,libdrm*,mesa*,xorg-x11-drv-a
-
+sudo dnf install discord
 sudo su
 cd /
 sudo touch /swapfile
@@ -15,3 +15,4 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo "Done"
+sudo reboot
