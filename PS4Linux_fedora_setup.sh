a0e169415c82d5ca27ce7ee7cc3e07dd7d2e6404
TigerClips1 <tigerclips1@localhost.localdomain>
2023-05-08 15:19:12 -0400

    PS4Linux

---
+++ PS4Linux_fedora_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "running the script"
+
+
+#first command
+sudo dnf update -x kernel*,libdrm*,mesa*,xorg-x11-drv-a
+
+sudo su
+cd /
+sudo touch /swapfile
+sudo fallocate -l 8G /swapfile -x
+chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo "Done"
---
+++ README.md
@@ -0,0 +1,9 @@
+so i Made this script to setup fedora on PS4Linux
+what it's doing is it updateing the distro and make 8GB of swap space
+1. open a termnal
+2. cd where the PS4Linux_fedora_setup.sh located
+3. chmod +x PS4Linux_fedora_setup.sh
+4. sudo ./PS4Linux_fedora_setup.sh
+5. make sure you on fedora ps4 linux
+That it 
+Thanks for Reading
